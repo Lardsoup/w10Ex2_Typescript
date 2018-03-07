e0861babcda9035a2483fc0f1894998f5e566d57
Lardsoup <Nicki@sonic.dk>
2018-03-07 13:59:42 +0100

    Think its done? slight issue with Constructors in OverdraftAccount and LoanAccount must call super() - the constructor from BankAccount.

--- typescriptClasses.ts
+++ typescriptClasses.ts
@@ -1,50 +1,88 @@
 interface IBankAccount
 {
-    accountNumber: number;
-    balance: number;
-    firstName: string;
-    lastName: string;
-    rateOfInterest: number;
-    ssn: number;
-    addInterest(fee?: number);
-    deposit(amount: number)
-    getBalance(): number;
-    withdraw(amount: number);
+	accountNumber: number;
+	balance: number;
+	firstName: string;
+	lastName: string;
+	rateOfInterest: number;
+	ssn: number;
+	addInterest(fee?: number);
+	deposit(amount: number)
+	getBalance(): number;
+	withdraw(amount: number);
 }
 
 class BankAccount implements IBankAccount
 {
-    accountNumber: number;
-    balance: number;
-    firstName: string;
-    lastName: string;
-    rateOfInterest: number;
-    ssn: number;
+	accountNumber: number;
+	balance: number;
+	firstName: string;
+	lastName: string;
+	rateOfInterest: number;
+	ssn: number;
+	static nextAccountNumber: number;
 
-    addInterest(fee)
-    {
-        if(fee = null)
-        {
-            this.balance += 5;
-        }
-        else
-        {
-            this.balance += fee;
-        }
-    }
+	constructor(ssn: number, firstName: string, lastName: string, rateOfInterest?: number)
+	{
+		this.ssn = ssn;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.balance = 0;
 
-    deposit(amount)
-    {
-        this.balance += amount;
-    }
+		if(rateOfInterest == null)
+		{
+			this.rateOfInterest = 1.25;
+		}
+		else
+		{
+			this.rateOfInterest = rateOfInterest;
+		}
 
-    getBalance()
-    {
-        return this.balance;
-    }
+		this.accountNumber = BankAccount.nextAccountNumber;
+		BankAccount.nextAccountNumber++;
+	}
+	
 
-    withdraw(amount)
-    {
-        this.balance -= amount;
-    }
+	addInterest(fee)
+	{
+		if(fee == null)
+		{
+			this.balance += 5;
+		}
+		else
+		{
+			this.balance += fee;
+		}
+	}
+
+	deposit(amount)
+	{
+		this.balance += amount;
+	}
+
+	getBalance()
+	{
+		return this.balance;
+	}
+
+	withdraw(amount)
+	{
+		this.balance -= amount;
+	}
+}
+
+abstract class OverdraftAccount extends BankAccount
+{
+	overdraftInterest: number;
+	overdraftLimit: number;
+
+	checkLimit(): Boolean
+	{
+		return true;
+	}
+}
+
+abstract class LoanAccount extends BankAccount
+{
+	principal: number;
 }
